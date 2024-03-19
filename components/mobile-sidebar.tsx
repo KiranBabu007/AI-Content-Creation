4b43608db74227d8e177f6f7b0e342c1fd67d5d0
Kiran Babu <kiranbabu3355@gmail.com>
2024-03-19 22:02:54 +0530

    Refactor dashboard page and layout components

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -10,18 +10,27 @@ import Sidebar from "@/components/sidebar";
 export const MobileSidebar = ({
 
 }) => {
+    const [isMounted, setIsMounted] = useState(false);
+
+    useEffect(() => {
+        setIsMounted(true);
+    }, []);
+
+    if (!isMounted) {
+        return null;
+    }
 
 
-  return (
-    <Sheet>
-      <SheetTrigger>
-        <Button variant="ghost" size="icon" className="md:hidden">
-          <Menu />
-        </Button>
-      </SheetTrigger>
-      <SheetContent side="left" className="p-0">
-        <Sidebar  />
-      </SheetContent>
-    </Sheet>
-  );
+    return (
+        <Sheet>
+            <SheetTrigger>
+                <Button variant="ghost" size="icon" className="md:hidden">
+                    <Menu />
+                </Button>
+            </SheetTrigger>
+            <SheetContent side="left" className="p-0">
+                <Sidebar />
+            </SheetContent>
+        </Sheet>
+    );
 };
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -6,6 +6,7 @@ import Image from "next/image"
 import { cn } from "@/lib/utils";
 import { Montserrat } from "next/font/google";
 import { ImagePlusIcon, LayoutDashboard, MessageSquareCodeIcon, Settings } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const poppins = Montserrat({ weight: '600', subsets: ['latin'] });
 
@@ -36,6 +37,9 @@ const routes = [
 ]
 
 const Sidebar = () => {
+
+    const pathname = usePathname()
+
     return (
         <div className="space-y-4 py-4 flex flex-xol h-full bg-[#0D0F10]  text-white">
             <div className="px-3 py-2 flex-1">
@@ -60,6 +64,7 @@ const Sidebar = () => {
                             href={route.href}
                             className={cn(
                                 "text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition",
+                                pathname === route.href ? "text-white bg-white/10" : "text-zinc-400"
 
                             )}
                         >

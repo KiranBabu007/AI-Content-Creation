a23e2b51c95669fd49af6dd8aafb9cfe0e72d3dd
Kiran Babu <kiranbabu3355@gmail.com>
2024-03-19 17:44:56 +0530

    Add @radix-ui/react-dialog and MobileSidebar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button"
 import { UserButton } from "@clerk/nextjs"
 import { Menu } from "lucide-react"
+import { MobileSidebar } from "@/components/mobile-sidebar"
 
 const navbar = () => {
     return (
         <div className="flex items-center p-4 ">
             <Button variant="ghost" size="icon" className="md:hidden"   >
-                <Menu />
+                <MobileSidebar />
             </Button>
             <div className="flex w-full justify-end">
                 <UserButton afterSignOutUrl="/" />

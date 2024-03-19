fe3a6e7c907121b4c1dede69f9b11885be502773
Kiran Babu <kiranbabu3355@gmail.com>
2024-03-19 12:55:52 +0530

    Remove page component and add ClerkProvider to layout

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,9 @@
+
+
+const DashboardPage = () => {
+    return (
+        <div className="bg-slate-400">Dashboard</div>
+    )
+}
+
+export default DashboardPage
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-
-
-const page = () => {
-  return (
-    <div className="bg-slate-400">page</div>
-  )
-}
-
-export default page

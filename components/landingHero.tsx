d0379cca07c401f167305462d25829343774b350
Kiran Babu <kiranbabu3355@gmail.com>
2024-03-21 23:54:09 +0530

    Update landingHero styling

--- components/landingHero.tsx
+++ components/landingHero.tsx
@@ -10,10 +10,10 @@ export const LandingHero = () => {
     const { isSignedIn } = useAuth();
 
     return (
-        <div className="text-white font-bold py-15 pb-1 text-center space-y-5">
+        <div className="text-white font-bold py-15 sm:py-30 pb-1 text-center space-y-5">
             <div className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl space-y-5 font-extrabold">
                 <h1>The Best AI Tool for</h1>
-                <div className="text-transparent bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
+                <div className="text-transparent p-3 bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
                     <TypewriterComponent
                         options={{
                             strings: [

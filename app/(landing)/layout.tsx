d629060e6c942b7fb2e2e24f3f9153dec231bbbb
Kiran Babu <kiranbabu3355@gmail.com>
2024-03-21 19:56:59 +0530

    Refactor landing page component

---
+++ app/(landing)/layout.tsx
@@ -0,0 +1,14 @@
+
+const layout = ({ children }: {
+    children: React.ReactNode
+}) => {
+    return (
+        <main className="h-full bg-[#16191C] overflow-auto">
+            <div className="mx-auto max-w-screen-xl h-full w-full">
+                {children}
+            </div>
+        </main>
+    )
+}
+
+export default layout
--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,24 +1,8 @@
-import { Button } from "@/components/ui/button"
-import Link from 'next/link'
-import { UserButton } from "@clerk/nextjs"
 
 
 const LandingPage = () => {
-    return <div>
-        <div>
-            <h1>Landing Page(Unprotected)</h1>
-        </div>
-        <div>
-            <Link href='/sign-in'>
-                <Button>sign-in</Button>
-            </Link>
-            <Link href='/sign-up'>
-                <Button>sign-up</Button>
-            </Link>
-
-            <UserButton />
-
-        </div>
+    return <div className="h-full">
+        <LandingNavbar />
 
     </div>
 }
